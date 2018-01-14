9153e717d9d943d84984b4fd2cd1e8329f8be258
Lionel Desorme <lionel.desorme@free.fr>
2018-01-14 15:29:34 +0100

    Initial commit

---
+++ CheckDisk/CheckDirectory.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using log4net;
+
+namespace CheckDisk
+{
+    public class CheckDirectory
+    {
+        private static readonly ILog _logger = LogManager.GetLogger("CheckDir");
+
+        private readonly Random _rnd;
+
+        private List<string> _fileNames;
+
+        public int Count { get { return _fileNames.Count; } }
+
+        public CheckDirectory()
+        {
+            _fileNames = new List<string>();
+            _rnd = new Random();
+        }
+
+        // Process all files in the directory passed in, recurse on any directories 
+        // that are found, and process the files they contain.
+        public void ProcessDirectory(string targetDirectory)
+        {
+            // Process the list of files found in the directory.
+            string[] fileEntries = Directory.GetFiles(targetDirectory);
+            foreach (string fileName in fileEntries)
+                ProcessFile(fileName);
+
+            // Recurse into subdirectories of this directory.
+            string[] subdirectoryEntries = Directory.GetDirectories(targetDirectory);
+            foreach (string subdirectory in subdirectoryEntries)
+                ProcessDirectory(subdirectory);
+        }
+
+        // Insert logic for processing found files here.
+        public void ProcessFile(string path)
+        {
+            //Console.WriteLine("Processed file '{0}'.", path);
+            _fileNames.Add(path);
+            if (_fileNames.Count%100==0)
+                _logger.Info($"{_fileNames.Count} files processed.");
+        }
+
+        public void ReadFile()
+        {
+            try
+            {
+                int r = _rnd.Next(_fileNames.Count);
+                var fileName = _fileNames[r];
+                _logger.Info($"will treat {fileName}...");
+
+                FileInfo fi = new FileInfo(fileName);
+
+                int p = _rnd.Next((int)fi.Length);
+                var buffer = new byte[16];
+                
+                using (var f = File.OpenRead(fileName))
+                {
+                    f.Seek(p, SeekOrigin.Begin);
+                    int nb=f.Read(buffer, 0, 16);
+                    _logger.Info($"Read {nb} bytes.");
+                }
+            }
+            catch (Exception e)
+            {
+                _logger.Info(e);
+            }
+        }
+    }
+}
---
+++ CheckDisk/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CheckDisk
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length<=0)
+            {
+                Console.WriteLine("Syntaxe: checkDisk directory1 directory2 ...");
+                return;
+            }
+            int delay = 10*1000;
+            var cd = new CheckDirectory();
+            foreach (var d in args)
+            {
+                Console.WriteLine($"Processing dir {d}");
+                cd.ProcessDirectory(d);
+                Console.WriteLine($"{cd.Count} total files processed.");
+            }
+            while (true)
+            {
+                cd.ReadFile();
+                Thread.Sleep(delay);
+            }
+        }
+    }
+}
