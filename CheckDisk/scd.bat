581bce3656c1b730e60418a14c44b35a7199d366
lionel-git <lionel.desorme@free.fr>
2018-01-31 18:57:53 +0100

    Add auto start in service script

--- CheckDisk/scd.bat
+++ CheckDisk/scd.bat
@@ -1,5 +1,5 @@
 rem @echo off
 set SVCNAME=CheckDisk
 sc.exe delete %SVCNAME%
-sc.exe create %SVCNAME% binPath= %~dp0\%SVCNAME%.exe DisplayName= %SVCNAME%
+sc.exe create %SVCNAME% binPath= %~dp0\%SVCNAME%.exe DisplayName= %SVCNAME% start= auto
 sc.exe description %SVCNAME% "Regularly poll some disk drives"
