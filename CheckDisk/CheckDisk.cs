9a72e93be053c743fdff1ac60b74ba8389ab2b4f
Lionel Desorme <lionel.desorme@free.fr>
2018-01-31 15:07:07 +0100

    Start testing service

--- CheckDisk/CheckDisk.cs
+++ CheckDisk/CheckDisk.cs
@@ -4,19 +4,61 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using ServiceUtils;
+using System.Threading;
+using log4net;
+using System.Configuration;
 
 namespace CheckDisk 
 {
     class CheckDisk : IService
     {
+        private static readonly ILog _logger = LogManager.GetLogger("CheckDisk");
+        private Task _task;
+        private CheckDirectory _checkDirectory;
+        private int _delay;
+        volatile bool _running = true;
+
+        private void MainLoop()
+        {
+            while (_running)
+            {
+                _checkDirectory.ReadFile();
+                Thread.Sleep(_delay);
+            }
+            _logger.Info("Exiting main loop");
+        }
+
         public void OnStart(string[] args)
         {
-            throw new NotImplementedException();
+            var directories = ConfigurationManager.AppSettings["Directories"].ToString().Split(',').ToList();
+            directories.AddRange(args);
+
+            _delay = 10 * 1000;
+            _checkDirectory = new CheckDirectory();
+            foreach (var d in directories)
+            {
+                _logger.Info($"Processing dir {d}");
+                try
+                {
+                    _checkDirectory.ProcessDirectory(d);
+                }
+                catch (Exception e)
+                {
+                    _logger.Info(e);
+                }
+                _logger.Info($"{_checkDirectory.Count} total files processed.");
+            }
+            _logger.Info($"Files used: {_checkDirectory.Count}");
+
+            _task = new Task(new Action(MainLoop));
+            _task.Start();
         }
 
         public void OnStop()
         {
-            throw new NotImplementedException();
+            _running = false;
+           // Thread.Sleep(_delay);
+            _logger.Info("Exiting On Stop");
         }
     }
 }
--- CheckDisk/Program.cs
+++ CheckDisk/Program.cs
@@ -5,34 +5,16 @@ using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using log4net;
+using System.Configuration;
+using ServiceUtils;
 
 namespace CheckDisk
 {
     class Program
     {
-        private static readonly ILog _logger = LogManager.GetLogger("Program");
-
         static void Main(string[] args)
         {
-            if (args.Length<=0)
-            {
-                Console.WriteLine("Syntaxe: checkDisk directory1 directory2 ...");
-                return;
-            }
-            int delay = 10*1000;
-            var cd = new CheckDirectory();
-            foreach (var d in args)
-            {
-                Console.WriteLine($"Processing dir {d}");
-                cd.ProcessDirectory(d);
-                Console.WriteLine($"{cd.Count} total files processed.");
-            }
-            _logger.Info($"Files used: {cd.Count}");
-            while (true)
-            {
-                cd.ReadFile();
-                Thread.Sleep(delay);
-            }
+            Starter<CheckDisk>.Start("CheckDisk", args);
         }
     }
 }
