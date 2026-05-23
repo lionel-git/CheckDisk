03fb6e8be6020d81f72caadb5a1fe34a710f7ac9
Lionel Desorme <lionel.desorme@free.fr>
2026-05-23 11:32:26 +0200

    change test service to on demand

--- CheckDisk/scd_test.bat
+++ CheckDisk/scd_test.bat
@@ -1,5 +1,5 @@
 rem @echo off
 set SVCNAME=CheckDiskTest
 sc.exe delete %SVCNAME%
-sc.exe create %SVCNAME% binPath= %~dp0\CheckDisk.exe DisplayName= %SVCNAME% start= auto
+sc.exe create %SVCNAME% binPath= %~dp0\CheckDisk.exe DisplayName= %SVCNAME% start= demand
 sc.exe description %SVCNAME% "Regularly poll some disk drives, test version"
